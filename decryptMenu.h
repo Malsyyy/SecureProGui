97973484aa23a82216bb5f18959474c3c230787e
Mals Fenwick <270279190@yoobeestudent.ac.nz>
2025-06-06 11:58:14 +1200

    updating files
    updating file

--- decryptMenu.h
+++ decryptMenu.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 namespace WindowsForm {
 
@@ -23,87 +23,105 @@ namespace WindowsForm {
 		System::ComponentModel::Container^ components;
 		System::Windows::Forms::Label^ welcomeMsg;
 		System::Windows::Forms::Button^ browseBtn;
-		System::Windows::Forms::Button^ decryptBtn;
-		System::Windows::Forms::Button^ logoutBtn;
-		System::Windows::Forms::Button^ exitBtn;
+	private: System::Windows::Forms::Button^ encryptBtn;
+
+		   System::Windows::Forms::Button^ logoutBtn;
+		   System::Windows::Forms::Button^ exitBtn;
+	private: System::Windows::Forms::RichTextBox^ fileBox;
+
+
+		   System::Windows::Forms::OpenFileDialog^ openFileDialog;
 
 #pragma region Windows Form Designer generated code
-		void InitializeComponent(void)
-		{
-			this->welcomeMsg = (gcnew System::Windows::Forms::Label());
-			this->browseBtn = (gcnew System::Windows::Forms::Button());
-			this->decryptBtn = (gcnew System::Windows::Forms::Button());
-			this->logoutBtn = (gcnew System::Windows::Forms::Button());
-			this->exitBtn = (gcnew System::Windows::Forms::Button());
+		   void InitializeComponent(void)
+		   {
+			   this->welcomeMsg = (gcnew System::Windows::Forms::Label());
+			   this->browseBtn = (gcnew System::Windows::Forms::Button());
+			   this->encryptBtn = (gcnew System::Windows::Forms::Button());
+			   this->logoutBtn = (gcnew System::Windows::Forms::Button());
+			   this->exitBtn = (gcnew System::Windows::Forms::Button());
+			   this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
+			   this->fileBox = (gcnew System::Windows::Forms::RichTextBox());
+			   this->SuspendLayout();
+			   // 
+			   // welcomeMsg
+			   // 
+			   this->welcomeMsg->Location = System::Drawing::Point(58, 220);
+			   this->welcomeMsg->Name = L"welcomeMsg";
+			   this->welcomeMsg->Size = System::Drawing::Size(0, 100);
+			   this->welcomeMsg->TabIndex = 4;
+			   // 
+			   // browseBtn
+			   // 
+			   this->browseBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			   this->browseBtn->Location = System::Drawing::Point(472, 188);
+			   this->browseBtn->Name = L"browseBtn";
+			   this->browseBtn->Size = System::Drawing::Size(164, 49);
+			   this->browseBtn->TabIndex = 5;
+			   this->browseBtn->Text = L"Browse For A file:";
+			   this->browseBtn->Click += gcnew System::EventHandler(this, &decryptMenu::browseBtn_Click);
+			   // 
+			   // encryptBtn
+			   // 
+			   this->encryptBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			   this->encryptBtn->Location = System::Drawing::Point(250, 275);
+			   this->encryptBtn->Name = L"encryptBtn";
+			   this->encryptBtn->Size = System::Drawing::Size(164, 49);
+			   this->encryptBtn->TabIndex = 6;
+			   this->encryptBtn->Text = L"Upload File";
+			   // 
+			   // logoutBtn
+			   // 
+			   this->logoutBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			   this->logoutBtn->Location = System::Drawing::Point(169, 475);
+			   this->logoutBtn->Name = L"logoutBtn";
+			   this->logoutBtn->Size = System::Drawing::Size(169, 49);
+			   this->logoutBtn->TabIndex = 7;
+			   this->logoutBtn->Text = L"Logout";
+			   // 
+			   // exitBtn
+			   // 
+			   this->exitBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			   this->exitBtn->Location = System::Drawing::Point(369, 475);
+			   this->exitBtn->Name = L"exitBtn";
+			   this->exitBtn->Size = System::Drawing::Size(169, 49);
+			   this->exitBtn->TabIndex = 8;
+			   this->exitBtn->Text = L"Exit";
+			   // 
+			   // fileBox
+			   // 
+			   this->fileBox->Location = System::Drawing::Point(169, 192);
+			   this->fileBox->Name = L"fileBox";
+			   this->fileBox->Size = System::Drawing::Size(297, 45);
+			   this->fileBox->TabIndex = 9;
+			   this->fileBox->Text = L"";
+			   // 
+			   // encryptMenu
+			   // 
+			   this->ClientSize = System::Drawing::Size(807, 589);
+			   this->Controls->Add(this->fileBox);
+			   this->Controls->Add(this->welcomeMsg);
+			   this->Controls->Add(this->browseBtn);
+			   this->Controls->Add(this->encryptBtn);
+			   this->Controls->Add(this->logoutBtn);
+			   this->Controls->Add(this->exitBtn);
+			   this->Name = L"decryptMenu";
+			   this->Text = L"Decrypt Menu";
+			   this->ResumeLayout(false);
 
-			this->SuspendLayout();
-
-			// 
-			// welcome Message
-			// 
-
-			this->welcomeMsg->Location = System::Drawing::Point(58, 20);
-			this->welcomeMsg->Name = L"welcomeMsg";
-			this->welcomeMsg->Size = System::Drawing::Size(300, 30);
-			this->welcomeMsg->TabIndex = 0;
-			this->welcomeMsg->Text = L"Welcome to the Decrypt Menu";
-
-			// 
-			// browse Button
-			// 
-			this->browseBtn->Location = System::Drawing::Point(472, 188);
-			this->browseBtn->Name = L"browseBtn";
-			this->browseBtn->Size = System::Drawing::Size(164, 49);
-			this->browseBtn->TabIndex = 1;
-			this->browseBtn->Text = L"Browse For A File";
-			this->browseBtn->UseVisualStyleBackColor = true;
-
-			// 
-			// decrypt Button
-			// 
-			this->decryptBtn->Location = System::Drawing::Point(250, 275);
-			this->decryptBtn->Name = L"decryptBtn";
-			this->decryptBtn->Size = System::Drawing::Size(164, 49);
-			this->decryptBtn->TabIndex = 2;
-			this->decryptBtn->Text = L"Decrypt File";
-			this->decryptBtn->UseVisualStyleBackColor = true;
-
-			// 
-			// logout Button
-			// 
-			this->logoutBtn->Location = System::Drawing::Point(169, 475);
-			this->logoutBtn->Name = L"logoutBtn";
-			this->logoutBtn->Size = System::Drawing::Size(169, 49);
-			this->logoutBtn->TabIndex = 3;
-			this->logoutBtn->Text = L"Logout";
-			this->logoutBtn->UseVisualStyleBackColor = true;
-
-			// 
-			// exit Button
-			// 
-			this->exitBtn->Location = System::Drawing::Point(369, 475);
-			this->exitBtn->Name = L"exitBtn"; // ✅ Fixed from "logoutBtn"
-			this->exitBtn->Size = System::Drawing::Size(169, 49);
-			this->exitBtn->TabIndex = 4;
-			this->exitBtn->Text = L"Exit";
-			this->exitBtn->UseVisualStyleBackColor = true;
-
-			// 
-			// decryptMenu 
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(807, 589);
-			this->Controls->Add(this->welcomeMsg);
-			this->Controls->Add(this->browseBtn);
-			this->Controls->Add(this->decryptBtn);
-			this->Controls->Add(this->logoutBtn);
-			this->Controls->Add(this->exitBtn);
-			this->Name = L"decryptMenu";
-			this->Text = L"Decrypt Menu";
-			this->ResumeLayout(false);
-		}
+		   }
 #pragma endregion
+
+	private: System::Void browseBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+		openFileDialog->Title = "Select a file to upload";
+		openFileDialog->Filter = "All Files (*.*)|*.*";
+
+		if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			System::String^ filePath = openFileDialog->FileName;
+			System::Windows::Forms::MessageBox::Show("Selected file: " + filePath);
+		}
+	}
+
 
 	};
 }
--- encryptMenu.h
+++ encryptMenu.h
@@ -23,107 +23,105 @@ namespace WindowsForm {
 		System::ComponentModel::Container^ components;
 		System::Windows::Forms::Label^ welcomeMsg;
 		System::Windows::Forms::Button^ browseBtn;
-		System::Windows::Forms::Button^ uploadBtn;
+	private: System::Windows::Forms::Button^ encryptBtn;
+
 		System::Windows::Forms::Button^ logoutBtn;
 		System::Windows::Forms::Button^ exitBtn;
-		System::Windows::Forms::OpenFileDialog^ openFileDialog;
+	private: System::Windows::Forms::RichTextBox^ fileBox;
+
+
+		   System::Windows::Forms::OpenFileDialog^ openFileDialog;
 
 #pragma region Windows Form Designer generated code
 		void InitializeComponent(void)
 		{
-			this->SuspendLayout();
-			this->ClientSize = System::Drawing::Size(300, 200);
-			this->Name = L"encryptMenu";
-			this->Text = L"Encrypt Menu";
-			this->ResumeLayout(false);
 			this->welcomeMsg = (gcnew System::Windows::Forms::Label());
-			this->browseBtn =  (gcnew System::Windows::Forms::Button());
-			this->uploadBtn =  (gcnew System::Windows::Forms::Button());
-			this->logoutBtn =  (gcnew System::Windows::Forms::Button());
-			this->exitBtn =    (gcnew System::Windows::Forms::Button());
-			this->openFileDialog = gcnew System::Windows::Forms::OpenFileDialog();
-			this->uploadBtn->Click += gcnew System::EventHandler(this, &encryptMenu::uploadBtn_Click);
-
-
-			//
-			// Welcome message
-			//
-
+			this->browseBtn = (gcnew System::Windows::Forms::Button());
+			this->encryptBtn = (gcnew System::Windows::Forms::Button());
+			this->logoutBtn = (gcnew System::Windows::Forms::Button());
+			this->exitBtn = (gcnew System::Windows::Forms::Button());
+			this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->fileBox = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// welcomeMsg
+			// 
 			this->welcomeMsg->Location = System::Drawing::Point(58, 220);
 			this->welcomeMsg->Name = L"welcomeMsg";
 			this->welcomeMsg->Size = System::Drawing::Size(0, 100);
 			this->welcomeMsg->TabIndex = 4;
-
-			//
-			// Browse Button
-			//
-
-			this->browseBtn->Location = System::Drawing::Point(472,188) ;
+			// 
+			// browseBtn
+			// 
+			this->browseBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->browseBtn->Location = System::Drawing::Point(472, 188);
 			this->browseBtn->Name = L"browseBtn";
 			this->browseBtn->Size = System::Drawing::Size(164, 49);
+			this->browseBtn->TabIndex = 5;
 			this->browseBtn->Text = L"Browse For A file:";
-			this->browseBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
-
-			//
-			// Upload Button
-			//
-
-			this->uploadBtn->Location = System::Drawing::Point(250, 275);
-			this->uploadBtn->Name = L"uploadBtn";
-			this->uploadBtn->Text = L"Upload File";
-			this->uploadBtn->Size = System::Drawing::Size(164,49);
-			this->uploadBtn-> Anchor = System::Windows::Forms::AnchorStyles::Top;
-
-			//
-			// Logout Button
-			//
-	
-			this->logoutBtn->Location = System::Drawing::Point(169 ,475 );
-			this->logoutBtn->Name = L"logoutBtn";
-			this->logoutBtn->Text = L"Logout";
-			this->logoutBtn->Size = System::Drawing::Size(169,49);
+			this->browseBtn->Click += gcnew System::EventHandler(this, &encryptMenu::browseBtn_Click);
+			// 
+			// encryptBtn
+			// 
+			this->encryptBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
+			this->encryptBtn->Location = System::Drawing::Point(250, 275);
+			this->encryptBtn->Name = L"encryptBtn";
+			this->encryptBtn->Size = System::Drawing::Size(164, 49);
+			this->encryptBtn->TabIndex = 6;
+			this->encryptBtn->Text = L"Upload File";
+			// 
+			// logoutBtn
+			// 
 			this->logoutBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
-
-
-			//
-			// Exit Button
-			//
-
-			this->exitBtn->Location = System::Drawing::Point(369, 475);
-			this->exitBtn->Name = L"logoutBtn";
-			this->exitBtn->Text = L"Exit";
-			this->exitBtn->Size = System::Drawing::Size(169, 49);
+			this->logoutBtn->Location = System::Drawing::Point(169, 475);
+			this->logoutBtn->Name = L"logoutBtn";
+			this->logoutBtn->Size = System::Drawing::Size(169, 49);
+			this->logoutBtn->TabIndex = 7;
+			this->logoutBtn->Text = L"Logout";
+			// 
+			// exitBtn
+			// 
 			this->exitBtn->Anchor = System::Windows::Forms::AnchorStyles::Top;
-
-			//
-			// Encrypt Menu Controls
-			//
-
-			this->ClientSize = System::Drawing::Size(807,589);
+			this->exitBtn->Location = System::Drawing::Point(369, 475);
+			this->exitBtn->Name = L"exitBtn";
+			this->exitBtn->Size = System::Drawing::Size(169, 49);
+			this->exitBtn->TabIndex = 8;
+			this->exitBtn->Text = L"Exit";
+			// 
+			// fileBox
+			// 
+			this->fileBox->Location = System::Drawing::Point(169, 192);
+			this->fileBox->Name = L"fileBox";
+			this->fileBox->Size = System::Drawing::Size(297, 45);
+			this->fileBox->TabIndex = 9;
+			this->fileBox->Text = L"";
+			// 
+			// encryptMenu
+			// 
+			this->ClientSize = System::Drawing::Size(807, 589);
+			this->Controls->Add(this->fileBox);
 			this->Controls->Add(this->welcomeMsg);
 			this->Controls->Add(this->browseBtn);
-			this->Controls->Add(this->uploadBtn);
+			this->Controls->Add(this->encryptBtn);
 			this->Controls->Add(this->logoutBtn);
 			this->Controls->Add(this->exitBtn);
-			this->Name = L"EncryptMenu";
+			this->Name = L"encryptMenu";
 			this->Text = L"Encrypt Menu";
+			this->ResumeLayout(false);
 
-
-
-				;
 		}
 #pragma endregion
 
-private: System::Void uploadBtn_Click(System::Object^ sender, System::EventArgs^ e) {
-	openFileDialog->Title = "Select a file to upload";
-	openFileDialog->Filter = "All Files (*.*)|*.*";
+	private: System::Void browseBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+		openFileDialog->Title = "Select a file to upload";
+		openFileDialog->Filter = "All Files (*.*)|*.*";
 
-	if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
-		System::String^ filePath = openFileDialog->FileName;
-		System::Windows::Forms::MessageBox::Show("Selected file: " + filePath);
+		if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK) {
+			System::String^ filePath = openFileDialog->FileName;
+			System::Windows::Forms::MessageBox::Show("Selected file: " + filePath);
+		}
 	}
-}
 
 
-};
+	};
 }
